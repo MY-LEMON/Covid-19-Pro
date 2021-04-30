20641f83bed5befeb7154739812cbbf6eb747eae
Geralt <superj0529@gmail.com>
2021-04-30 14:05:43 +0800

    selfservice page generated

--- frontend_vue/src/views/SelfService.vue
+++ frontend_vue/src/views/SelfService.vue
@@ -1,10 +1,27 @@
 <template>
-<p>SelfService</p>
+  <div>
+    <p>nihao?</p>
+    <el-radio-group v-model="radio">
+      <el-radio :label="3" @click="test">备选项</el-radio>
+      <el-radio :label="6" @click="test">备选项</el-radio>
+      <el-radio :label="9" @select="test">备选项</el-radio>
+    </el-radio-group>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "SelfService"
+  name: "SelfService",
+  data(){
+    return{
+      radio: 0
+    }
+  },
+  methods:{
+    test(){
+      console.log(this.radio)
+    }
+  }
 }
 </script>
 
