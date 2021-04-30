1870af68ac28599ab800f4443d2c491a793f3220
Geralt <superj0529@gmail.com>
2021-04-30 14:06:07 +0800

    simplify searchapi

--- frontend_vue/src/apis/search.js
+++ frontend_vue/src/apis/search.js
@@ -1,13 +1,14 @@
-import request from '@/utils/request'
-
+import axios from "axios";
+// import request from '@/utils/request'
 export function doSearch(key){
-    return request({
-        url: '/',
-        method: 'post',
-        data: {
-            key: key
-        }
-    })
+    // return request({
+    //     url: '/',
+    //     method: 'post',
+    //     data: {
+    //         key: key
+    //     }
+    // })
+    return axios.get(key)
 }
 
 
