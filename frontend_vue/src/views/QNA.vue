6c774cdbf2d76fe55f4db1b4bdd5a2803a754f3b
Geralt <superj0529@gmail.com>
2021-05-01 17:04:05 +0800

    update the UI

--- frontend_vue/src/views/QNA.vue
+++ frontend_vue/src/views/QNA.vue
@@ -1,42 +1,80 @@
 <template>
-  <div class="div" v-if="isloaded">
-    <Kgraph  v-bind:graph_json_data="graphdata"></Kgraph>
-  </div>
+
+    <el-container>
+    <el-header>
+      <el-input placeholder="请输入问题" v-model="input" onfocus="select">
+
+        <el-button @click="submit" slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </el-header>
+    <el-container v-if="isloaded">
+
+      <el-aside width="30%">{{textdata}}</el-aside>
+      <el-main><Kgraph  v-bind:graph_json_data="graphdata" ></Kgraph></el-main>
+    </el-container>
+    </el-container>
+
+
+
 </template>
 
 <script>
-import {doSearch} from "@/apis/search";
+// import {doSearch} from "@/apis/search";
 import Kgraph from "../components/kgraph.vue";
+import axios from "axios";
 export default {
   name: 'QNA',
   props:{
     graphdata:{
       type: Object,
       require: true
+    },
+    textdata:{
+      type: Object,
+      require: true
     }
   },
   data(){
     return{
+      input:'',
       isloaded: false
     }
   },
 beforeMount() {
     console.log("QNA page mounting")
-  doSearch("https://b2f65068-d22f-4c9e-81c6-2faa7d5cb2bd.mock.pstmn.io/qna").then(resp=>{
-    console.log("loading data")
-    this.graphdata = resp.data.data[1]
-    this.isloaded = true
-  })
+
 },
   mounted() {
     console.log("QNA page mounted")
   },
   components: {
     Kgraph
+  },
+  methods: {
+    submit(){
+      this.isloaded = false
+      axios.get("https://b2f65068-d22f-4c9e-81c6-2faa7d5cb2bd.mock.pstmn.io/qna",{
+        params:{
+          "key":this.input
+        }
+      }).then(resp=>{
+        console.log("loading data")
+        this.graphdata = resp.data.data[1]
+        this.textdata = resp.data.data[0]
+        this.isloaded = true
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.left {
+  float: left;
+  width: 200px;
+  height: 100%;
+}
+.right {
+  margin-left: 200px;
+}
 </style>
