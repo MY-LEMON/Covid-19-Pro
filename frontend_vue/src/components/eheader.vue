2ed520c4734e4f2ab4cb0bd02b54832f102c4760
Geralt <superj0529@gmail.com>
2021-05-01 18:40:42 +0800

    fixed header index bug

--- frontend_vue/src/components/eheader.vue
+++ frontend_vue/src/components/eheader.vue
@@ -1,17 +1,17 @@
 <template>
   <div style="margin-top: 0;margin-bottom: 10px">
     <el-menu
-        :default-active="activeIndex2"
+        :default-active="this.$parent.$route.path"
         class="el-menu-demo"
         mode="horizontal"
         @select="handleSelect"
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b">
-      <el-menu-item index="1" @click="jumpto('/')">主页</el-menu-item>
-      <el-menu-item index="2" @click="jumpto('/News')">新闻</el-menu-item>
-      <el-menu-item index="3" @click="jumpto('/QNA')">问答</el-menu-item>
-      <el-menu-item index="4" @click="jumpto('/SelfService')">自诊</el-menu-item>
+      <el-menu-item index="/" @click="jumpto('/')">主页</el-menu-item>
+      <el-menu-item index="/News" @click="jumpto('/News')">新闻</el-menu-item>
+      <el-menu-item index="/QNA" @click="jumpto('/QNA')">问答</el-menu-item>
+      <el-menu-item index="/SelfService" @click="jumpto('/SelfService')">自诊</el-menu-item>
 
     </el-menu>
   </div>
@@ -22,8 +22,7 @@ export default {
   name: 'eheader',
   data () {
     return {
-      activeIndex: '1',
-      activeIndex2: '1'
+
     }
   },
   methods: {
