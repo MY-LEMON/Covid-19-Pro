1889a7566935f5753f0725a876f57b0d71f35c52
Geralt <superj0529@gmail.com>
2021-05-02 23:06:04 +0800

    add two pie-chart in to home page

--- frontend_vue/src/views/Home.vue
+++ frontend_vue/src/views/Home.vue
@@ -5,7 +5,16 @@
 
       </el-aside>
       <el-main v-if="this.mapisloaded">
-        <map-china v-bind:china="chinadata" v-bind:province="provincedata"></map-china>
+        <el-row >
+          <map-china v-bind:china="chinadata" v-bind:province="provincedata"></map-china>
+
+        </el-row>
+        <el-row>
+          <pie-chart v-bind:resq="provincePieData"></pie-chart>
+        </el-row>
+        <el-row>
+          <piechart_foreign v-bind:resq="countryPieData"></piechart_foreign>
+        </el-row>
       </el-main>
 
   </el-container>
@@ -17,22 +26,32 @@
 // import Kgraph from "./components/kgraph";
 import mapChina from "@/components/mapChina.vue";
 import axios from "axios";
+import pieChart from "@/components/pieChart"
+import piechart_foreign from "@/components/pieChart_foreign";
 export default {
   name: 'Home',
   data(){
     return{
-      chinadata:{},
+      chinadata:[],
       provincedata:{},
+      provincePieData:{},
+      countryPieData:{},
       mapisloaded:false
     }
   },
   components: {
-    mapChina
+
+    mapChina,
+    pieChart,
+    piechart_foreign
   },
   mounted() {
+    this.mapisloaded = false
   axios.get("http://172.22.69.121:5000/").then(resp=>{
     this.chinadata = resp.data.data["China"]
     this.provincedata = resp.data.data["Provinces"]
+    this.provincePieData = resp.data.data["pie_province"]
+    this.countryPieData = resp.data.data["bar_country"]
     this.mapisloaded = true
   })
   },
