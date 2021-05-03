380d706fe5223475fef8f31d178c4d395c2a9bb1
Geralt <superj0529@gmail.com>
2021-05-03 22:12:00 +0800

    add two rank-bars

--- frontend_vue/src/views/Home.vue
+++ frontend_vue/src/views/Home.vue
@@ -4,19 +4,25 @@
       <el-main v-if="this.mapisloaded">
         <el-row class="mainbox">
           <el-col :span="6" class="leftbox">
-
+            <el-row>
               <pie-chart v-bind:resq="provincePieData" style="width: 100%;"></pie-chart>
-
+            </el-row>
+            <el-row>
+              <rank v-bind:resq="rankData" style="width: 100%;"></rank>
+            </el-row>
           </el-col>
           <el-col :span="12" class="middlebox">
 
-          <map-china v-bind:china="chinadata" v-bind:province="provincedata" style="width: 100%;"></map-china>
+            <map-china v-bind:china="chinadata" v-bind:province="provincedata" style="width: 100%;"></map-china>
 
           </el-col>
           <el-col :span="6" class="rightbox" >
-
+            <el-row>
               <piechart_foreign v-bind:resq="countryPieData" style="width: 100%;"></piechart_foreign>
-
+            </el-row>
+            <el-row>
+              <rank_foreign v-bind:resq="rankForeignData" style="width: 100%;"></rank_foreign>
+            </el-row>
           </el-col>
         </el-row>
 
@@ -34,6 +40,8 @@ import mapChina from "@/components/mapChina.vue";
 import axios from "axios";
 import pieChart from "@/components/pieChart"
 import piechart_foreign from "@/components/pieChart_foreign";
+import rank from "@/components/rank";
+import rank_foreign from "@/components/rank_foreign";
 export default {
   name: 'Home',
   data(){
@@ -42,6 +50,8 @@ export default {
       provincedata:{},
       provincePieData:{},
       countryPieData:{},
+      rankData:{},
+      rankForeignData:{},
       mapisloaded:false
     }
   },
@@ -49,7 +59,9 @@ export default {
 
     mapChina,
     pieChart,
-    piechart_foreign
+    piechart_foreign,
+    rank,
+    rank_foreign
   },
   mounted() {
     this.mapisloaded = false
@@ -58,6 +70,8 @@ export default {
     this.provincedata = resp.data.data["Provinces"]
     this.provincePieData = resp.data.data["pie_province"]
     this.countryPieData = resp.data.data["bar_country"]
+    this.rankData = resp.data.data["bar_province"]
+    this.rankForeignData = resp.data.data["bar_country"]
     this.mapisloaded = true
   })
   },
